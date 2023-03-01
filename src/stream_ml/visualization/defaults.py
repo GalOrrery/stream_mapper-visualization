3fc101b80a708949b3927e65b4978a2eae37e397
Nathaniel Starkman <nstarman@users.noreply.github.com>
2023-03-01 14:29:49 -0500

    more ylabel defaults (#15)
    Signed-off-by: nstarman <nstarkman@protonmail.com>

--- src/stream_ml/visualization/defaults.py
+++ src/stream_ml/visualization/defaults.py
@@ -5,13 +5,31 @@ __all__: list[str] = []
 
 
 YLABEL_DEFAULTS = {
-    "phi1": "$\\phi_1$",
-    "phi2": r"$\phi_2$",
-    "plx": r"$\varpi$",
+    # ICRS - RA
+    "ra": r"$\alpha$",
+    # ICRS - Dec
+    "dec": r"$\delta$",
+    # Distance
+    "distance": r"$r$",
     "parallax": r"$\varpi$",
+    "plx": r"$\varpi$",
+    # ICRS - Proper Motion
+    "pmra": r"$\mu_{\alpha}$",
+    "pmra_cosdec": r"$\mu_{\alpha}^*$",
+    "pmra_cosdec_unrefl": r"$\mu_{\alpha}^*$",
+    "pmra_cosdec_refl": r"$\mu_{\alpha}^*$",
+    "pmdec": r"$\mu_{\delta}$",
+    "pmdec_unrefl": r"$\mu_{\delta}^*$",
+    "pmdec_refl": r"$\mu_{\delta}^*$",
+    "radial_velocity": r"$v_r$",
+    # Custom
+    "phi1": r"$\phi_1$",
+    "phi2": r"$\phi_2$",
     "pm_phi1": r"$\mu_{phi_1}$",
     "pm_phi1_cosphi2": r"$\mu_{phi_1}^*$",
     "pm_phi1_cosphi2_unrefl": r"$\mu_{phi_1}^*$",
+    "pm_phi1_cosphi2_refl": r"$\mu_{phi_1}^*$",
     "pm_phi2": r"$\mu_{phi_2}$",
     "pm_phi2_unrefl": r"$\mu_{phi_2}$",
+    "pm_phi2_refl": r"$\mu_{phi_2}$",
 }
